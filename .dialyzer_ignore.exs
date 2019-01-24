459f9ddd9809f8e1d61b60760299bffc37c5a460
TheFirstAvenger <mike@mikebinns.net>
2019-01-24 16:46:32 -0500

    Dialyzer fix

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,3 +1,6 @@
 [
-  ~r/Function :httpc\.request\/4 does not exist\./
+  ~r/Function :httpc\.request\/4 does not exist\./,
+  # https://github.com/elixir-lang/elixir/issues/8631
+  ~r/Function with_deps\/0 has no local return/,
+  ~r/Function with_deps\/1 has no local return/
 ]
